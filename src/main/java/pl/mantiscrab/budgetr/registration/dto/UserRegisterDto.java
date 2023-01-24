eb60a016ac7641965bd8e91978a5b27d59244b12
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-01-24 11:06:27 +0100

    Add integration test

--- src/main/java/pl/mantiscrab/budgetr/registration/dto/UserRegisterDto.java
+++ src/main/java/pl/mantiscrab/budgetr/registration/dto/UserRegisterDto.java
@@ -1,8 +1,11 @@
 package pl.mantiscrab.budgetr.registration.dto;
 
+import lombok.Builder;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+@Builder
 public record UserRegisterDto(
         @Email @NotBlank String email,
         @NotBlank String username,
