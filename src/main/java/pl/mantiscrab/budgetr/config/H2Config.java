326295a2643af9fc50762c056508545b61611f41
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-02-03 17:02:29 +0100

    Configure H2 database

---
+++ src/main/java/pl/mantiscrab/budgetr/config/H2Config.java
@@ -0,0 +1,15 @@
+package pl.mantiscrab.budgetr.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+class H2Config {
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp");
+    }
+}
