8cc989bed2c78876dc89036d0bc5342366da71cd
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-02-21 09:26:42 +0100

    Implement RecentlyAuthenticatedUsers interfaces

---
+++ src/main/java/pl/mantiscrab/budgetr/auth/config/AuthenticationEventPublisherConfig.java
@@ -0,0 +1,16 @@
+package pl.mantiscrab.budgetr.auth.config;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+
+@Configuration
+class AuthenticationEventPublisherConfig {
+    @Bean
+    public AuthenticationEventPublisher authenticationEventPublisher
+            (ApplicationEventPublisher applicationEventPublisher) {
+        return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
+    }
+}
