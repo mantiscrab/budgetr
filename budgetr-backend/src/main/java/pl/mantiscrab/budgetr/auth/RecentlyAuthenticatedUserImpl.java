0a67bdeac511df76fe29759d71bca8437c7a3f24
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-02-24 09:54:02 +0100

    Remove auth package and security dependencies

--- budgetr-backend/src/main/java/pl/mantiscrab/budgetr/auth/RecentlyAuthenticatedUserImpl.java
+++
@@ -1,7 +0,0 @@
-package pl.mantiscrab.budgetr.auth;
-
-import pl.mantiscrab.budgetr.domain.user.infrastructure.RecentlyAuthenticatedUser;
-
-record RecentlyAuthenticatedUserImpl(String username, String email) implements RecentlyAuthenticatedUser {
-
-}
