19b6b934c05137fb018bb696f19a343cf0ffc792
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-02-27 07:54:16 +0100

    DummyRecentlyAuthenticatedUsersPublisherImpl

--- budgetr-backend/src/main/java/pl/mantiscrab/budgetr/auth/DummyRecentlyAuthenticatedUsersPublisherImpl.java
+++ budgetr-backend/src/main/java/pl/mantiscrab/budgetr/auth/DummyRecentlyAuthenticatedUsersPublisherImpl.java
@@ -1,8 +1,8 @@
-//package pl.mantiscrab.budgetr.auth;
-//
-//import org.springframework.stereotype.Service;
-//import pl.mantiscrab.budgetr.domain.user.infrastructure.RecentlyAuthenticatedUsersPublisher;
-//
-//@Service
-//class DummyRecentlyAuthenticatedUsersPublisherImpl extends RecentlyAuthenticatedUsersPublisher {
-//}
+package pl.mantiscrab.budgetr.auth;
+
+import org.springframework.stereotype.Service;
+import pl.mantiscrab.budgetr.domain.user.infrastructure.RecentlyAuthenticatedUsersPublisher;
+
+@Service
+class DummyRecentlyAuthenticatedUsersPublisherImpl extends RecentlyAuthenticatedUsersPublisher {
+}
