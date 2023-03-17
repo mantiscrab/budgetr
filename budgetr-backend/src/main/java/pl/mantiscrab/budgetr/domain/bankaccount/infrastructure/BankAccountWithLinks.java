9c4511a083e12a9d777bff9fb2348963bec0907f
mantiscrab <93212605+mantiscrab@users.noreply.github.com>
2023-03-17 15:35:41 +0100

    Fix tests

--- budgetr-backend/src/main/java/pl/mantiscrab/budgetr/domain/bankaccount/infrastructure/BankAccountWithLinks.java
+++ budgetr-backend/src/main/java/pl/mantiscrab/budgetr/domain/bankaccount/infrastructure/BankAccountWithLinks.java
@@ -4,10 +4,12 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 import pl.mantiscrab.budgetr.domain.bankaccount.dto.BankAccountDto;
 
+import java.math.BigDecimal;
 import java.net.URI;
 
 import static pl.mantiscrab.budgetr.domain.bankaccount.infrastructure.BankAccountLinksProvider.bankAccountByIdLink;
@@ -16,9 +18,16 @@ import static pl.mantiscrab.budgetr.domain.bankaccount.infrastructure.BankAccoun
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Relation(collectionRelation = "bankAccounts")
-class BankAccountWithLinks extends RepresentationModel<BankAccountWithLinks> {
+public class BankAccountWithLinks extends RepresentationModel<BankAccountWithLinks> {
 
     private final BankAccountDto bankAccount;
+
+    /**
+     * Constructor needed for proper deserialization in tests
+     **/
+    private BankAccountWithLinks(BankAccountDto bankAccount, Iterable<Link> initialLinks) {
+        this.bankAccount = bankAccount;
+    }
 
     static BankAccountWithLinks of(BankAccountDto bankAccountDto) {
         final BankAccountWithLinks bankAccountWithLinks = new BankAccountWithLinks(bankAccountDto);
@@ -30,5 +39,17 @@ class BankAccountWithLinks extends RepresentationModel<BankAccountWithLinks> {
 
     URI getSelfUri() {
         return this.getLink(IanaLinkRelations.SELF).orElseThrow().toUri();
+    }
+
+    public Long id() {
+        return bankAccount.id();
+    }
+
+    public String name() {
+        return bankAccount.name();
+    }
+
+    public BigDecimal initialBalance() {
+        return bankAccount.initialBalance();
     }
 }
--- budgetr-backend/src/test/java/pl/mantiscrab/budgetr/domain/bankaccount/BankAccountHelper.java
+++ budgetr-backend/src/test/java/pl/mantiscrab/budgetr/domain/bankaccount/BankAccountHelper.java
@@ -2,22 +2,24 @@ package pl.mantiscrab.budgetr.domain.bankaccount;
 
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import pl.mantiscrab.budgetr.UriProvider;
 import pl.mantiscrab.budgetr.domain.bankaccount.dto.BankAccountDto;
 import pl.mantiscrab.budgetr.domain.bankaccount.infrastructure.BankAccountController;
+import pl.mantiscrab.budgetr.domain.bankaccount.infrastructure.BankAccountWithLinks;
 
 import java.net.URI;
-import java.util.List;
 
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
 
 public class BankAccountHelper {
     private final TestRestTemplate restTemplate;
     private final UriProvider uriProvider;
-    private final ParameterizedTypeReference<List<BankAccountDto>> typeReference = new ParameterizedTypeReference<>() {
+    private final ParameterizedTypeReference<CollectionModel<BankAccountWithLinks>> bankAccountWithLinksCollectionModel
+            = new ParameterizedTypeReference<>() {
     };
 
     public BankAccountHelper(TestRestTemplate testRestTemplate, int localServerPort) {
@@ -25,26 +27,26 @@ public class BankAccountHelper {
         uriProvider = new UriProvider(localServerPort);
     }
 
-    public ResponseEntity<BankAccountDto> createAccount(String username, String password, BankAccountDto bankAccountDto) {
+    public ResponseEntity<BankAccountWithLinks> createAccount(String username, String password, BankAccountDto bankAccountDto) {
         URI uri = uriProvider.getUriOn(on(BankAccountController.class).createBankAccount(null));
-        return restTemplate.withBasicAuth(username, password).postForEntity(uri, bankAccountDto, BankAccountDto.class);
+        return restTemplate.withBasicAuth(username, password).postForEntity(uri, bankAccountDto, BankAccountWithLinks.class);
     }
 
-    public ResponseEntity<List<BankAccountDto>> getAccounts(String username, String password) {
+    public ResponseEntity<CollectionModel<BankAccountWithLinks>> getAccounts(String username, String password) {
         URI uri = uriProvider.getUriOn(on(BankAccountController.class).getAccounts());
         return restTemplate.withBasicAuth(username, password)
-                .exchange(uri, HttpMethod.GET, null, typeReference);
+                .exchange(uri, HttpMethod.GET, null, bankAccountWithLinksCollectionModel);
     }
 
-    public ResponseEntity<BankAccountDto> getAccount(String username, String password, Long id) {
+    public ResponseEntity<BankAccountWithLinks> getAccount(String username, String password, Long id) {
         URI uri = uriProvider.getUriOn(on(BankAccountController.class).getAccountById(id));
-        return restTemplate.withBasicAuth(username, password).getForEntity(uri, BankAccountDto.class);
+        return restTemplate.withBasicAuth(username, password).getForEntity(uri, BankAccountWithLinks.class);
     }
 
-    public ResponseEntity<BankAccountDto> updateAccount(String username, String password, BankAccountDto bankAccountDto) {
+    public ResponseEntity<BankAccountWithLinks> updateAccount(String username, String password, BankAccountDto bankAccountDto) {
         URI uri = uriProvider.getUriOn(on(BankAccountController.class).updateBankAccount(bankAccountDto.id(), bankAccountDto));
         RequestEntity<BankAccountDto> requestEntity = new RequestEntity<>(bankAccountDto, HttpMethod.PUT, uri);
-        return restTemplate.withBasicAuth(username, password).exchange(requestEntity, BankAccountDto.class);
+        return restTemplate.withBasicAuth(username, password).exchange(requestEntity, BankAccountWithLinks.class);
     }
 
     ResponseEntity<Void> deleteAccount(String username, String password, Long id) {
